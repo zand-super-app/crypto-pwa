fdf936672c57988285183c5ee3d79b0d255d0731
Sharaf <sharaf.innovazions@gmail.com>
2023-10-10 22:27:02 +0400

    feat: theme changes + accept user details from host app

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -8,12 +8,24 @@ import { Component, Input } from '@angular/core';
 export class ExploreContainerComponent {
   @Input() name?: string;
 
+  userData: any;
+  welcomeMessage: string = "";
+
+  ngOnInit() {
+    window.addEventListener('message', (event) => {
+      if (event.data.includes('userName')) {
+        this.userData = JSON.parse(event.data);     
+        this.welcomeMessage = this.userData.fullName;  
+      }
+    });
+  }
+
   MyCoins = [
     {
       icon: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Solana_logo.png',
       title: 'Solana',
       quantity: '50 SOL',
-      currencyValue: 'AED 836',
+      currencyValue: 'AED 836.00',
       change: '10.05',
       changeType: 'up',
     },
@@ -21,15 +33,15 @@ export class ExploreContainerComponent {
       icon: 'https://ethereum.org/static/5022dfe8d23b106be4d3d2b021accd46/8cb4c/eth-diamond-black-gray.png',
       title: 'Ethereum',
       quantity: '2 ETH',
-      currencyValue: 'AED 3827',
+      currencyValue: 'AED 3827.00',
       change: '12.07',
       changeType: 'up',
     },
     {
-      icon: 'https://altcoinsbox.com/wp-content/uploads/2023/03/matic-logo.png',
+      icon: 'https://logowik.com/content/uploads/images/polygon-matic-icon3725.logowik.com.webp',
       title: 'Polygon',
       quantity: '140 MATIC',
-      currencyValue: 'AED 92',
+      currencyValue: 'AED 92.00',
       change: '2.87',
       changeType: 'up',
     },
